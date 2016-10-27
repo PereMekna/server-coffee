286ee8860f5d6d5afd2ae90fc99433f6cb127872
PereMekna <dupont.louis4@gmail.com>
2016-10-28 00:27:39 +0200

    update server

--- server/boot/update-model.js
+++ server/boot/update-model.js
@@ -1,5 +1,8 @@
-/*module.exports = function(app) {
+module.exports = function(app) {
   app.dataSources.postgres.automigrate('User', function(err) {
     if (err) throw err;
   });
-};*/
+  app.dataSources.postgres.automigrate('Coffee', function(err) {
+    if (err) throw err;
+  });
+};
