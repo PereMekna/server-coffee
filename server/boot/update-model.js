1920fa1b218665ac71bf99395d88ace5af638778
PereMekna <dupont.louis4@gmail.com>
2016-10-28 10:05:09 +0200

    add cookies

--- server/boot/update-model.js
+++ server/boot/update-model.js
@@ -1,8 +1,8 @@
-module.exports = function(app) {
+/*module.exports = function(app) {
   app.dataSources.postgres.automigrate('User', function(err) {
     if (err) throw err;
   });
   app.dataSources.postgres.automigrate('CoffeeShop', function(err) {
     if (err) throw err;
   });
-};
+};*/
